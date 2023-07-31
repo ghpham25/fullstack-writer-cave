da300af347a09595527cf6c18ad9000192db619a
Giang Pham <giangpham@Giangs-MacBook-Air.local>
2023-07-31 10:13:26 -0400

    first commit

---
+++ writer_cave/src/App.js
@@ -0,0 +1,17 @@
+import Quill from "./Quill";
+import Test from "./test";
+import Test4 from "./Test4";
+
+function App() {
+  return (
+    <>
+      <h2 className="text-red-500"> This is from App </h2>
+      <Test> </Test>
+      <h3> prettier test</h3>
+      <Test4> </Test4>
+      <Quill/>
+    </>
+  );
+}
+
+export default App;
---
+++ writer_cave/src/test.js
@@ -0,0 +1,7 @@
+function Test() {
+    return (
+        <h1 className = "text-blue-600"> This is test header </h1>
+    )
+}
+
+export default Test; 
