4a778e7e095ff48167717192f674890f35f5ff46
Giang Pham <giangpham@Giangs-MacBook-Air.local>
2023-08-01 14:51:35 -0400

    added submit button

--- writer_cave/src/Question.jsx
+++ writer_cave/src/Question.jsx
@@ -11,11 +11,21 @@ export default function Header() {
     setIsPromptRevealed(true);
   };
 
+  const [isTextSubmitted, setIsTextSubmitted] = useState(false);
+
+  //need to change to disableText, save the Text in a separate file, 
+  //and show text like "Thank you for filling today's prompt, come back 
+  //tomorrow for more prompt"
+
+  const disableText = () => {
+    setIsTextSubmitted(true);
+  };
   return (
     <>
       <div className="font-loader">
         {/* Question */}
-        <div id="question" className="flex mt-2">
+        {
+        !isPromptRevealed && (<div id="question" className="flex mt-2">
           <label
             htmlFor="revealButtons"
             className="ml-5 py-2 px-2 text-white font-bold text-lg"
@@ -25,7 +35,9 @@ export default function Header() {
           </label>
 
           {/* Yes-No Buttons */}
+    
           <div id="revealButtons" className="">
+            
             <button
               onClick={showPrompt}
               className="bg-blue-500 focus:bg-blue-800 border-none text-white on-clv hover:bg-blue-800 reveal w-10 py-2 px-2 rounded-lg mr-2"
@@ -39,24 +51,45 @@ export default function Header() {
               No{" "}
             </button>
           </div>
-        </div>
+        </div>)
+        }
 
         {/*Text prompt and text box for answering */}
         <div id="answer">
-          {isPromptRevealed && (
-            <div id="prompt" className="block ml-5 mt-5">
+          {isPromptRevealed && !isTextSubmitted && (
+            <div id="prompt" className="block ml-5 mt-2">
               <span className="text-white"> {textPrompt} </span>
             </div>
           )}
-          {isPromptRevealed && (
+
+          {isPromptRevealed && !isTextSubmitted && (
             <div class="relative mb-3 ml-3 mr-2" data-te-input-wrapper-init>
               <textarea
                 id="message"
                 rows="4"
-                class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                class="h-80 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Write your thoughts here..."
+                disabled={isTextSubmitted}
+                
               ></textarea>
             </div>
+          )}
+
+          {isPromptRevealed && !isTextSubmitted && (
+            <div className="text-right mr-3">
+              {" "}
+              <button
+                onClick={disableText}
+                className="px-2 py-2 bg- bg-blue-800  hover:bg-blue-950 text-white rounded-xl"
+              >
+                {" "}
+                Save and stop editting{" "}
+              </button>{" "}
+            </div>
+          )}
+
+          {isTextSubmitted && (
+            <h2 className = " flex justify-center wrap text-white text-center font-bold text-3xl mt-10"> Thanks for sharing your thoughts today! Come back tomorrow for more prompts ~</h2>
           )}
         </div>
       </div>
