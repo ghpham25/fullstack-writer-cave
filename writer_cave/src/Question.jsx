69d5ae7b10ce15b944782436d0bb5043959f3824
Giang Pham <giangpham@Giangs-MacBook-Air.local>
2023-08-01 12:36:32 -0400

    fixed the issue

--- writer_cave/src/Question.jsx
+++ writer_cave/src/Question.jsx
@@ -1,23 +1,21 @@
 import React, { useState } from "react";
 
 export default function Header() {
+  let textPrompt = "This is an example prompt";
 
-  // let textPrompt = "This is an example prompt"
-  
-  const [textPrompt, setText] = useState("")
+  const [isPromptRevealed, setIsPromptRevealed] = useState(false);
   const showPrompt = () => {
     // display the Prompt
-    let newPrompt = "This is an example prompt"
-    setText(newPrompt)
+    // let newPrompt = "This is an example prompt";
+    // setText(newPrompt);
+    setIsPromptRevealed(true);
   };
-
-  const textBox = ""
 
   return (
     <>
       <div className="font-loader">
         {/* Question */}
-        <div className="flex">
+        <div id="question" className="flex mt-2">
           <label
             htmlFor="revealButtons"
             className="ml-5 py-2 px-2 text-white font-bold text-lg"
@@ -44,21 +42,23 @@ export default function Header() {
         </div>
 
         {/*Text prompt and text box for answering */}
-        <div id = "answer">
-          <div id="prompt" className="block ml-5 mt-5">
-            <span className  ="text-white"> {textPrompt} </span>
-          </div>
-
-      <div class="relative mb-3" data-te-input-wrapper-init>
-        <textarea
-          class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-          id="exampleFormControlTextarea1"
-          rows="4"
-          ></textarea>
-      </div>
-
+        <div id="answer">
+          {isPromptRevealed && (
+            <div id="prompt" className="block ml-5 mt-5">
+              <span className="text-white"> {textPrompt} </span>
+            </div>
+          )}
+          {isPromptRevealed && (
+            <div class="relative mb-3 ml-3 mr-2" data-te-input-wrapper-init>
+              <textarea
+                id="message"
+                rows="4"
+                class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Write your thoughts here..."
+              ></textarea>
+            </div>
+          )}
         </div>
-
       </div>
     </>
   );
