1fb2bdd5da2c12d5598109dd5b2f17d4875713f7
Giang Pham <ghpham25@colby.edu>
2023-09-05 18:00:12 +0700

    frontend for login

--- writer_cave/client/src/App.js
+++ writer_cave/client/src/App.js
@@ -3,22 +3,31 @@ import Home from "./pages/Home";
 import Inspiration from "./pages/Inspiration";
 import MyWriting from "./pages/MyWriting";
 import FreeWriting from "./pages/FreeWriting";
-
+import Login from "./pages/Login";
 import Test from "./components/Test";
+
 import { Routes, Route } from 'react-router-dom';
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 function App() {
   document.body.style.backgroundColor = "#708dee";
+  const theme = createTheme({
+    typography: {
+      fontFamily: ["Chilanka", "cursive"].join(","),
+    },
+  });
+
   return (
-    <>
-      <Header />
+    <ThemeProvider theme = {theme}>
+      <Login/>
+      {/* <Header /> */}
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/Home" element={<Home />} />
         <Route path="/mywriting" element={<MyWriting />} />
         <Route path="/inspo" element={<Inspiration />} />
         <Route path="/freewrite" element={<FreeWriting />} />
       </Routes>
-    </>
+    </ThemeProvider>
   );
 }
 
